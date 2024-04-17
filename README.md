cc1754453869473f5573ec1d260ac5f59bcc70cb
irisss3099 <irisss3099@gmail.com>
2024-04-17 18:45:05 +0500

    push my word counter to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sk_word_counter" 
---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers : {
+    sentences: string 
+} = await inquirer.prompt([
+    {
+        name:"sentences",
+        type:"input",
+        message:"enter your sentence to count the word:"
+    }
+])
+const words = answers.sentences.trim().split(" ")
+
+console.log(chalk.yellow(words));
+
+console.log(chalk.bold.cyanBright.italic(`your sentence word is ${words.length} `));
